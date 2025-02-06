0913c8ea7581a1dde4dc9b32a1123e68a6e5bfb6
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-02-05 18:37:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("Thanks to Official Joke API");
+
+const jokeElement = document.getElementById("jokeElement");
+const favJokeDisplay = document.getElementById("favJokeDisplay");
+
+let favJokes = JSON.parse(localStorage.getItem("favJokes"));
+
+favJokeDisplay.textContent = favJokes.join(" || ");
+
+async function getJoke() {
+    try {
+        const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+
+        if (!response.ok) {
+            throw new Error("API could not be found");
+        }
+
+        return response.json();
+    } 
+
+    catch (error) {
+        console.error(error);
+    }
+}
+
+async function generateJoke() {
+    const data = await getJoke();
+    const joke = data.setup + " " + data.punchline;
+    
+    jokeElement.textContent = joke;
+}
+
+function favJoke() {
+        if(!favJokes.includes(jokeElement.textContent)) {
+            favJokes.push(jokeElement.textContent);
+            favJokeDisplay.textContent = favJokes.join(" || ");
+    
+            localStorage.setItem("favJokes", JSON.stringify(favJokes));
+        }
+}
+
+function removeJokes() {
+    favJokes = [];
+    localStorage.setItem("favJokes", JSON.stringify(favJokes));
+
+    favJokeDisplay.textContent = "";
+}
